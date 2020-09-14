26cfac442cfaef25f74532378c402183df047a0f
bess <bree.kuwa@gmail.com>
2020-09-15 03:37:35 +0900

    no message

---
+++ test-line_1.php
@@ -0,0 +1,33 @@
+
+
+{
+    "type": "imagemap",
+    "baseUrl": "スクリーンショット 2020-09-14 15.39.05",
+    "altText": "あひる",
+    "baseSize": {
+        "width": 1040,
+        "height": 731
+    },
+    "actions": [
+        {
+            "type": "message",
+            "text": "うい",
+            "area": {
+                "x":140,
+                "y":180,
+                "width":150,
+                "height":150
+            }
+        },
+        {
+            "type": "uri",
+            "linkUri": "https://symfoware.blog.fc2.com/",
+            "area": {
+                "x":670,
+                "y":70,
+                "width":190,
+                "height":170
+            }
+        }
+    ]
+}
